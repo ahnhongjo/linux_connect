21d2b8c331cdfc1baca4a68db49a7947ac2d22cd
hongjo <ahj0313@ajou.ac.kr>
2021-03-08 00:02:26 +0900

    test3

--- test/main.c
+++ test/main.c
@@ -3,8 +3,12 @@
 int main() {
     for(int i=1;i<10;i++)
     {
-        printf("%d ",i);
+        for(int j=1;j<10;j++){
+            printf("%d * %d = %2d  ",i,j,i*j);
+        }
+        printf("\n");
     }
+
 
 
 
