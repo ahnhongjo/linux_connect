997471b2e12688ad26213536409413821c0d445b
hongjo <ahj0313@ajou.ac.kr>
2021-03-09 03:37:58 +0900

    그래프 만들어봄

---
+++ test/AL_graph.h
@@ -0,0 +1,61 @@
+/*
+ * AL_graph.h
+ *
+ *  Created on: 2017. 5. 25.
+ *      Author: cobus
+ */
+
+#ifndef SRC_DATA_STRUCTURE_AL_GRAPH_H_
+#define SRC_DATA_STRUCTURE_AL_GRAPH_H_
+
+enum VisitMode {
+    Visited,
+    NotVisited
+};
+
+typedef int GraphElementType;
+
+typedef struct tagVertex {
+    GraphElementType data;
+    int isVisited;
+    int index;
+
+    struct tagVertex* next;
+    struct tagEdge* adjacencyList;
+} Vertex;
+
+typedef struct tagEdge {
+    int weight;
+    struct tagEdge* next;
+    Vertex* from;
+    Vertex* to;
+} Edge;
+
+typedef struct tagGraph {
+    Vertex* vertices;
+    int vertexCount;
+} Graph;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+Graph* createGraph();
+void destroyGraph(Graph* g);
+
+Vertex* createVertex(GraphElementType data);
+void destroyVertex(Vertex* v);
+
+Edge* createEdge(Vertex* from, Vertex* to, int weight);
+void destroyEdge(Edge* e);
+
+void addVertex(Graph* g, Vertex* v);
+void addEdge(Vertex* v, Edge* e);
+
+void printGraph(Graph* g);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* SRC_DATA_STRUCTURE_AL_GRAPH_H_ */
--- test/main.c
+++ test/main.c
@@ -1,16 +1,180 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "AL_graph.h"
 
-int main() {
-    for(int i=1;i<10;i++)
-    {
-        for(int j=1;j<10;j++){
-            printf("%d * %d = %2d  ",i,j,i*j);
-        }
-        printf("\n");
+Graph* createGraph() {
+    Graph* g = (Graph*) malloc(sizeof(Graph));
+    if (g == NULL) {
+        return NULL;
     }
 
+    g->vertexCount = 0;
+    g->vertices = NULL;
+    return g;
+}
 
+void destroyGraph(Graph* g) {
+    if (g == NULL) {
+        return;
+    }
 
+    while(g->vertices != NULL) {
+        Vertex* next = g->vertices->next;
+        destroyVertex(g->vertices);
+        g->vertices = next;
+    }
+
+    free(g);
+    g = NULL;
+}
+
+Vertex* createVertex(GraphElementType data) {
+    Vertex* v = (Vertex*)malloc(sizeof(Vertex));
+    if (v == NULL) {
+        return NULL;
+    }
+
+    v->data = data;
+    v->index = -1;
+    v->isVisited = NotVisited;
+    v->next = NULL;
+    v->adjacencyList = NULL;
+    return v;
+}
+
+void destroyVertex(Vertex* v) {
+    if (v == NULL) {
+        return;
+    }
+
+    while(v->adjacencyList != NULL) {
+        Edge* next = v->adjacencyList->next;
+        destroyEdge(v->adjacencyList);
+        v->adjacencyList = next;
+    }
+
+    free(v);
+    v = NULL;
+}
+
+Edge* createEdge(Vertex* from, Vertex* to, int weight) {
+    Edge* e = (Edge*) malloc(sizeof(Edge));
+    if (e == NULL) {
+        return NULL;
+    }
+
+    e->weight = weight;
+    e->from = from;
+    e->to = to;
+    e->next = NULL;
+    return e;
+}
+
+void destroyEdge(Edge* e) {
+    if (e == NULL) {
+        return;
+    }
+
+    free(e);
+    e = NULL;
+}
+
+void addVertex(Graph* g, Vertex* v) {
+    if (g == NULL || v == NULL) {
+        return;
+    }
+
+    Vertex* vList = g->vertices;
+    if (vList == NULL) {
+        g->vertices = v;
+    }
+    else {
+        while(vList->next != NULL) {
+            vList = vList->next;
+        }
+        vList->next = v;
+    }
+
+    v->index = g->vertexCount++;
+}
+
+void addEdge(Vertex* v, Edge* e) {
+    if (v == NULL || e == NULL) {
+        return;
+    }
+
+    Edge* eList = v->adjacencyList;
+    if (eList == NULL) {
+        v->adjacencyList = e;
+    }
+    else {
+        while(eList->next != NULL) {
+            eList = eList->next;
+        }
+        eList->next = e;
+    }
+}
+
+void printGraph(Graph* g) {
+    Vertex* v = NULL;
+    Edge* e = NULL;
+
+    v = g->vertices;
+    if (v == NULL) {
+        return;
+    }
+
+    while(v != NULL) {
+        printf("[% 2d][v:%d]->", v->index, v->data);
+
+        e = v->adjacencyList;
+        while (e != NULL) {
+            printf("[v:%d][w:%d]->", e->to->data, e->weight);
+            e = e->next;
+        }
+
+        printf("NULL\n");
+        v = v->next;
+    }
+}
+
+int main() {
+    Graph *g = createGraph();
+
+    Vertex* v1 = createVertex(1);
+    Vertex* v2 = createVertex(2);
+    Vertex* v3 = createVertex(3);
+    Vertex* v4 = createVertex(4);
+    Vertex* v5 = createVertex(5);
+
+    addVertex(g, v1);
+    addVertex(g, v2);
+    addVertex(g, v3);
+    addVertex(g, v4);
+    addVertex(g, v5);
+
+    addEdge(v1, createEdge(v1, v2, 0));
+    addEdge(v1, createEdge(v1, v3, 0));
+    addEdge(v1, createEdge(v1, v4, 0));
+    addEdge(v1, createEdge(v1, v5, 0));
+
+    addEdge(v2, createEdge(v2, v1, 0));
+    addEdge(v2, createEdge(v2, v3, 0));
+    addEdge(v2, createEdge(v2, v5, 0));
+
+    addEdge(v3, createEdge(v3, v1, 0));
+    addEdge(v3, createEdge(v3, v2, 0));
+
+    addEdge(v4, createEdge(v4, v1, 0));
+    addEdge(v4, createEdge(v4, v5, 0));
+
+    addEdge(v5, createEdge(v5, v1, 0));
+    addEdge(v5, createEdge(v5, v2, 0));
+    addEdge(v5, createEdge(v5, v4, 0));
+
+    printGraph(g);
+
+    destroyGraph(g);
 
     return 0;
 }
