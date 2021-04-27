642ce688dbbaf1cc5e90604bdfcce9ea2f167d83
hongjo <ahj0313@ajou.ac.kr>
2021-04-27 09:43:11 +0900

    asd

--- untitled/main.c
+++ untitled/main.c
@@ -1,6 +1,25 @@
-#include <stdio.h>
 
-int main() {
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <errno.h>
+#include <stdlib.h>
+#ifndef _WIN32
+#include <unistd.h>
+#else
+#include <io.h>
+#endif
+#include <string.h>
+#include <libpmem.h>
+
+/* Copying 4K at a time to pmem for this example */
+#define BUF_LEN 4096
+
+int main(int argc, char *argv[]) {
+
     printf("Hello, World!\n");
+    printf("%s",argv[0]);
     return 0;
 }
